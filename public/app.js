bd2945e0935762abc052ea557eaf178930286f97
arseniy96 <arsenzhar@gmail.com>
2016-02-19 23:38:22 +0300

    Play with localStorage variable

--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
 /**
  * Created by Dima on 17.02.2016.
  */
-var x = 1
 
 function something()
 {
+    var x = window.localStorage.getItem('aaa');
+    
+    window.localStorage.setItem('aaa', 555);
+
     alert(x);
-    x = x + 1
 }
