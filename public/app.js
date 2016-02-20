517adf651461c9744fc215843a08eb8a53e4ae9c
arseniy96 <arsenzhar@gmail.com>
2016-02-20 23:33:22 +0300

    Add 'remove' button

--- public/app.js
+++ public/app.js
@@ -2,10 +2,19 @@
  * Created by Dima on 17.02.2016.
  */
 
-function something(pizza)
+function add_to_cart(pizza)
 {
     var x = window.localStorage.getItem(pizza);
     x = x*1 + 1
+    window.localStorage.setItem(pizza, x);
+
+    alert(pizza + ': ' + x.toString());
+}
+
+function remove_to_cart(pizza)
+{
+    var x = window.localStorage.getItem(pizza);
+    x = x*1 - 1
     window.localStorage.setItem(pizza, x);
 
     alert(pizza + ': ' + x.toString());
