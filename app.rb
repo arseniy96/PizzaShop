a5dd7b2241a71349cda22af41c6fa88849525162
arseniy96 <arsenzhar@gmail.com>
2016-02-27 01:03:19 +0300

    Replace id with name pizza

--- app.rb
+++ app.rb
@@ -53,9 +53,12 @@ end
 
 post '/cart' do
   orders_input = params[:orders]
-  @orders = parse_orders_input orders_input
+  @items = parse_orders_input orders_input
+  @items.each do |item|
+    item[0] = @pizza.find(item[0]).title
+  end
 
-  erb "#{@orders.inspect}"
+  erb :cart
 end
 
 def parse_orders_input orders_input
